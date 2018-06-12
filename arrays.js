660916d79a50424f5324458019c408df4c686175
Dave Beck <dbeck2@outlook.com>
2018-06-12 01:15:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function destructivelyAddElementToEndOfArray(myArray4, element) {
 }
 function accessElementInArray(myArray5, index) {
   var myArray5 = [1, 2, 3, 4];
-  console.log(myArray5[3]);
+  console.log(myArray5[2]);
 }
 function destructivelyRemoveElementFromBeginningOfArray(myArray6, element) {
   var myArray6 = [1, 2, 3, 4];
