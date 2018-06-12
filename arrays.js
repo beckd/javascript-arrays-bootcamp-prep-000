4d299314a0383971ff3f5e716c169a983a806a82
Dave Beck <dbeck2@outlook.com>
2018-06-12 00:57:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,11 @@ function destructivelyAddElementToBeginningOfArray(myArray2, element) {
   myArray2.unshift(element);
   return myArray2;
 }
+function addElementToEndOfArray(myArray3, element) {
+  myArray = [...myArray4, element];
+  return myArray3;
+}
+function destructivelyAddElementToEndOfArray(myArray4, element) {
+  myArray4.push(element);
+  return myArray4
+}
