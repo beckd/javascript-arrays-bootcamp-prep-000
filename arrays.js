3fedb43c8a6f7837f8eadda8bab325d6070dba24
Dave Beck <dbeck2@outlook.com>
2018-06-12 00:44:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ]
 function addElementToBeginningOfArray(myArray, foo) {
   var myArray = [1, 2, 3]
-  myArray.push("foo");
+  myArray.unshift("foo");
 }
 function destructivelyAddElementToBeginningOfArray() {
 
